2f0ba65e918f0109b4c5a0abb5bffcda43603b19
HexDevourSoap <forgottenbabo@gmail.com>
2025-04-21 14:53:53 +0300

    added all the clicks

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,18 +1,28 @@
-import { HomePage } from "./pageObjects/HomePage";
+import { HomePage } from "../pageObjects/Homepage";
+
 
 describe('demoqa scenarios', () => {
   beforeEach(() => {
     cy.visit('https://demoqa.com/selectable')
 
-  })
-
+  });
+  
+  // Click “Grid” scenario
   it("Grid", () => {
     // Click Grid button
     HomePage.gridButton.click();
-    
-    
   });
 
+  // Click - “Two”, “Four”, “Six”, “Eight” scenario
+  it("Two, Four, Six, Eight", () => {
+    // Click Grid button
+    HomePage.gridButton.click();
+    // Click - “Two”, “Four”, “Six”, “Eight”
+    HomePage.twoButton.click();
+    HomePage.fourButton.click();
+    HomePage.sixButton.click();
+    HomePage.eightButton.click();
+  });
 
-
-})
+ 
+})  
--- cypress/pageObjects/HomePage.js
+++ cypress/pageObjects/HomePage.js
@@ -1,4 +1,4 @@
-import { BasePage } from "./pageObjects/BasePage";
+import { BasePage } from "./BasePage";
 
 export class HomePage extends BasePage {
     static get url() {
@@ -8,7 +8,20 @@ export class HomePage extends BasePage {
     static get gridButton() {
         return cy.get("#demo-tab-grid");
       }
+    
+    static get twoButton() {
+        return cy.get("#row1 > li:nth-child(2)").click();
+    }  
 
+    static get fourButton() {
+        return cy.get("#row2 > li:nth-child(1)").click();
+    }
 
-
+    static get sixButton() {
+        return cy.get("#row2 > li:nth-child(3)").click();
+    }
+    static get eightButton() {
+      return cy.get("#row3 > li:nth-child(2)").click();
+  }
+    
 }
